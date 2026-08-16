da7f9498f46f592caedf2ffc8258dce72c6f7fd9
Ujjwal <ujjwalrawat248@gmail.com>
2026-08-16 16:51:09 +0530

    sent first message to the topic : my-topic

--- kafka-basics/src/main/java/io/conductor/demo/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conductor/demo/kafka/ProducerDemo.java
@@ -1,5 +1,8 @@
 package io.conductor.demo.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,5 +19,22 @@ public class ProducerDemo {
 
         // key : value of properties
         properties.put("bootstrap.servers", "127.0.0.1:9092");
+
+        //setting producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        //creating producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // creating producer code
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("my-topic", "First Message");
+
+        //send data
+        producer.send(record);
+
+        // flush and close the producer
+        producer.flush();
+        producer.close();
     }
 }
